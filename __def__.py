86530a66d5ce31ecf5128f90c08749069aaed8a4
DrunkLeen <reza.df.x@gmail.com>
2022-12-02 13:18:21 +0100

    verbose mode, ui improved, custom timeout

--- __def__.py
+++ __def__.py
@@ -1,28 +1,47 @@
 import socket
 from IPy import IP
+from os import system, name
+
+def clearscreen():
+    if name == 'nt':
+        _ = system('cls')
+    elif name == 'posix':
+        _ = system('clear')
+    else:
+        _ = system('clear')
+
+def logo():
+    print('█▀▄ █▀█ █░█ █▄░█ █▄▀ █▀ █▀▀ ▄▀█ █▄░█ █▄░█ █▀▀ █▀█')
+    print('█▄▀ █▀▄ █▄█ █░▀█ █░█ ▄█ █▄▄ █▀█ █░▀█ █░▀█ ██▄ █▀▄')
 
 
-def port_scanner(ip_address,ip_range_start,ip_range_end):
+def port_scanner(ip_address,ip_range_start,ip_range_end,timeout,verbose):
     open_ports = ''
     open_ports_sum = 0
 
     for i in range(ip_range_start,ip_range_end):    
         try:
             sck = socket.socket()
-            sck.settimeout(0.1)
+            sck.settimeout(timeout)
             sck.connect((ip_finder(ip_address),i))
 
             try: # Try to get Port Banner
-                port_banner = get_banner(sck)
-                print(f'[+] {ip_address} | Scaning port > {i} open')
-                open_ports += f'+ {i} {port_banner}\n'
+                port_banner = get_banner(sck).decode()
+                if verbose == 'Active':
+                    print(f'[+] {ip_address} | Scaning port > {i} open')
+
+                open_ports += f'+ {i} {port_banner.strip("")}\n'
             except:
-                print(f'[+] {ip_address} | Scaning port > {i} open')
+                if verbose == 'Active':
+                    print(f'[+] {ip_address} | Scaning port > {i} open')
+
                 open_ports += f'+ {i}\n'
             open_ports_sum += 1
 
         except:
-            print(f'[-] {ip_address} | Scaning port > {i} closed')
+            if verbose == 'Active':
+                print(f'[-] {ip_address} | Scaning port > {i} closed')
+
     a = f'\n----------------> {ip_address} <----------------\n{open_ports_sum} Open Port:\n{open_ports}'
     return a
 
@@ -35,6 +54,3 @@ def ip_finder(url_ip):
         return url_ip
     except ValueError:
         return socket.gethostbyname(url_ip)
-
-    
-       
--- main.py
+++ main.py
@@ -1,10 +1,62 @@
 from __def__ import *
 import time, math
+import sys
+
+clearscreen()
+logo()
 
 target_set = input('[+] Enter the target/s (Slpit Targets Range with , ): ')
 target_set.strip(' ')
+
+clearscreen()
+logo()
+print('Target/s: %s' %target_set)
+
 target_ip_range = input('[+] Enter port/s (Slpit Port Range with - ): ')
 target_ip_range.strip(' ')
+
+clearscreen()
+logo()
+print('Target/s: %s' %target_set)
+print('Timeout value set on %s' %target_ip_range)
+
+while True:
+    target_ip_timeout = ''
+    target_ip_timeout = input('[+] Set Timeout (Default=0.5): ')
+    target_ip_timeout.strip(' ')
+    try:
+        if target_ip_timeout.isdigit() or type(target_ip_timeout) == str:
+            target_ip_timeout = float(target_ip_timeout)
+            break
+        elif target_ip_timeout == '':
+            target_ip_timeout = 0.5
+            break
+    except:
+        print('Unverified input!')
+
+clearscreen()
+logo()
+print('Target/s: %s' %target_set)
+print('Port Range: %s' %target_ip_range)
+print('Timeout: %s' %target_ip_timeout)
+
+while True:
+    verbosemode = input('[+] do you want to activate Verbose mode (Y/n): ')
+    verbosemode.strip(' ')
+    if verbosemode == 'y' or verbosemode == 'Y' or verbosemode == '':
+        verbosemode = 'Active'
+        break
+    elif verbosemode == 'N' or verbosemode == 'n':
+         verbosemode = 'Deactive'
+         break
+
+clearscreen()
+logo()
+print('Target/s: %s' %target_set)
+print('Timeout: %s' %target_ip_timeout)
+print('Port Range: %s' %target_ip_range)
+print('verbose mode: %s' %verbosemode)
+
 
 start_time = time.time()
 
@@ -15,19 +67,27 @@ else:
     target_ip_range_start = int(target_ip_range)
     target_ip_range_end = int(target_ip_range)+1
 
-# print(port_scanner(ip_finder(target_set),target_ip_range_start,target_ip_range_end))
 resualt = ''
 if ',' in target_set:
     for target_set_single in target_set.split(','):
-        print(f'\nstarting with {target_set_single} ...')
-        resualt += port_scanner(target_set_single,target_ip_range_start,target_ip_range_end)
+        print(f'\nscanning {target_set_single} ...')
+        resualt += port_scanner(target_set_single,target_ip_range_start,\
+            target_ip_range_end,target_ip_timeout,verbosemode)
 
 else:
-    print(f'\nstarting with {target_set} ...')
-    resualt = port_scanner(target_set,target_ip_range_start,target_ip_range_end)
+    print(f'\nscanning {target_set} ...')
+    resualt = port_scanner(target_set,target_ip_range_start,target_ip_range_end,\
+        target_ip_timeout,verbosemode)
 
 end_time = time.time()
 end_time = format(end_time-start_time)
+
+clearscreen()
+logo()
+print('Target/s: %s' %target_set)
+print('Timeout: %s' %target_ip_timeout)
+print('Port Range: %s' %target_ip_range)
+print('verbose mode: %s' %verbosemode)
 
 print(resualt)
 print(f"It took {round(float(end_time),1)} seconds to test ports")
